a2563dacd455fbbc9a3590b978d67a936a8d92aa
homegrownotes <homegrownotes@gmail.com>
2023-03-10 18:30:48 +0100

    Exercici 2

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 // Exercici 1
+/*function showJoke(){ // => Function to activate "fetch" function only when "click".
+    fetch('https://icanhazdadjoke.com/', { // => API-url.
+        headers: {
+            'Accept': 'application/json' // Header => "Type of data =>json".
+        }
+    })
+        .then(response => response.json()) // => API response.
+        .then(data => console.log(data.joke)) // => Data extract.
+        .catch(error => console.error(error)); // => Checking "errors".
+}
+
+var btn = document.getElementById('btn'); // => Get "DOM necessary elements"
+btn?.addEventListener('click', showJoke); // => Adding event to start "showJoke function".*/
 function showJoke() {
     fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -6,8 +19,10 @@ function showJoke() {
         }
     })
         .then(function (response) { return response.json(); }) // => API response.
-        .then(function (data) { return console.log(data.joke); }) // => Data extract.
-    ["catch"](function (error) { return console.error(error); }); // => Checking "errors".
+        .then(function (data) {
+        var jokeElement = document.getElementById("joke");
+        jokeElement.textContent = data.joke;
+    })["catch"](function (error) { return console.error(error); }); // => Checking "errors".
 }
 var btn = document.getElementById('btn'); // => Get "DOM necessary elements"
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', showJoke); // => Adding event to start "showJoke function".
+btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', showJoke); // => Adding event to start "showJoke function".*/
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 // Exercici 1
-function showJoke() { // => Function to activate "fetch" function only when "click".
+/*function showJoke(){ // => Function to activate "fetch" function only when "click".
 	fetch('https://icanhazdadjoke.com/', { // => API-url.
 		headers: {
 			'Accept': 'application/json' // Header => "Type of data =>json".
@@ -11,4 +11,28 @@ function showJoke() { // => Function to activate "fetch" function only when "cli
 }
 
 var btn = document.getElementById('btn'); // => Get "DOM necessary elements"
-btn?.addEventListener('click', showJoke); // => Adding event to start "showJoke function".
+btn?.addEventListener('click', showJoke); // => Adding event to start "showJoke function".*/
+
+// Exercici 2
+
+interface showJoke {
+	id: string;
+	joke: string;
+}
+function showJoke() {
+	fetch('https://icanhazdadjoke.com/', { // => API-url.
+		headers: {
+			'Accept': 'application/json' // Header => "Type of data =>json".
+		}
+	})
+		.then(response => response.json()) // => API response.
+		.then(data => {
+			const jokeElement = document.getElementById("joke")!;
+			jokeElement.textContent = data.joke;
+		})
+		.catch(error => console.error(error)); // => Checking "errors".
+	} 
+
+		var btn = document.getElementById('btn'); // => Get "DOM necessary elements"
+		btn?.addEventListener('click', showJoke); // => Adding event to start "showJoke function".*/
+
