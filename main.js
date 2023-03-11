1bc28b3f51f6960adaff7f21edf5a55caf320b9f
homegrownotes <homegrownotes@gmail.com>
2023-03-11 09:42:29 +0100

    Exercici 3

--- main.js
+++ main.js
@@ -12,17 +12,46 @@
 
 var btn = document.getElementById('btn'); // => Get "DOM necessary elements"
 btn?.addEventListener('click', showJoke); // => Adding event to start "showJoke function".*/
+var reportAcudits = []; // Defining the array.
 function showJoke() {
-    fetch('https://icanhazdadjoke.com/', {
+    var jokeElement = document.getElementById("joke");
+    fetch("https://icanhazdadjoke.com/", {
         headers: {
-            'Accept': 'application/json' // Header => "Type of data =>json".
+            Accept: "application/json"
         }
     })
-        .then(function (response) { return response.json(); }) // => API response.
+        .then(function (response) { return response.json(); })
         .then(function (data) {
-        var jokeElement = document.getElementById("joke");
+        // When showing the joke, show the punctuation buttons.
+        var button1 = document.getElementById("btn1");
+        if (button1 !== null) {
+            button1.style.display = "inline";
+        }
+        var button2 = document.getElementById("btn2");
+        if (button2 !== null) {
+            button2.style.display = "inline";
+        }
+        var button3 = document.getElementById("btn3");
+        if (button3 !== null) {
+            button3.style.display = "inline";
+        }
+        // Add the joke to the HTML element.
         jokeElement.textContent = data.joke;
-    })["catch"](function (error) { return console.error(error); }); // => Checking "errors".
+        // We generate the date in ISO format.
+        var date = new Date().toISOString();
+        // Add the joke to the reportAcudits array.
+        reportAcudits.push({ joke: data.joke, score: 0, date: date });
+    })["catch"](function (error) { return console.error(error); });
 }
-var btn = document.getElementById('btn'); // => Get "DOM necessary elements"
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', showJoke); // => Adding event to start "showJoke function".*/
+// Update the joke score in the "reportAcudits" array.
+function addScore(score) {
+    var currentJoke = reportAcudits[reportAcudits.length - 1];
+    currentJoke.score = score;
+    console.log(reportAcudits); // Update & show the array.
+}
+// Assign events to the scoring buttons.
+document.getElementById("btn1").addEventListener("click", function () { return addScore(1); });
+document.getElementById("btn2").addEventListener("click", function () { return addScore(2); });
+document.getElementById("btn3").addEventListener("click", function () { return addScore(3); });
+// Assign event to the button to load the next joke.
+document.getElementById("btn").addEventListener("click", showJoke);
--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ btn?.addEventListener('click', showJoke); // => Adding event to start "showJoke 
 
 // Exercici 2
 
-interface showJoke {
+/*interface showJoke {
 	id: string;
 	joke: string;
 }
@@ -35,4 +35,66 @@ function showJoke() {
 
 		var btn = document.getElementById('btn'); // => Get "DOM necessary elements"
 		btn?.addEventListener('click', showJoke); // => Adding event to start "showJoke function".*/
+
+// Exercici 3
+
+interface Joke {
+	id: string;
+	joke: string;
+  }
+  
+  const reportAcudits: { joke: string; score: number; date: string }[] = []; // Defining the array.
+  
+  function showJoke(): void {
+	const jokeElement = document.getElementById("joke")!;
+  
+	fetch("https://icanhazdadjoke.com/", {
+	  headers: {
+		Accept: "application/json",
+	  },
+	})
+	  .then((response) => response.json())
+	  .then((data) => {
+		// When showing the joke, show the punctuation buttons.
+		const button1 = document.getElementById("btn1");
+		if (button1 !== null) {
+  		button1.style.display = "inline";}
+		const button2 = document.getElementById("btn2");
+		if (button2 !== null) {
+		button2.style.display = "inline";
+		}
+		const button3 = document.getElementById("btn3");
+		if (button3 !== null) {
+		button3.style.display = "inline";	
+		}
+		// Add the joke to the HTML element.
+		jokeElement.textContent = data.joke;
+  
+		// We generate the date in ISO format.
+		const date = new Date().toISOString();
+  
+		// Add the joke to the reportAcudits array.
+		reportAcudits.push({ joke: data.joke, score: 0, date: date });
+	  })
+	  .catch((error) => console.error(error));
+  }
+  
+  	// Update the joke score in the "reportAcudits" array.
+  	function addScore(score: number): void {
+	const currentJoke = reportAcudits[reportAcudits.length - 1];
+	currentJoke.score = score;
+	console.log(reportAcudits); // Update & show the array.
+  }
+  
+  // Assign events to the scoring buttons.
+  document.getElementById("btn1")!.addEventListener("click", () => addScore(1));
+  document.getElementById("btn2")!.addEventListener("click", () => addScore(2));
+  document.getElementById("btn3")!.addEventListener("click", () => addScore(3));
+  
+  // Assign event to the button to load the next joke.
+  document.getElementById("btn")!.addEventListener("click", showJoke);
+  
+
+
+ 
 
